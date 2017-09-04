a2c76c1a7e3f2571f3ab4ee6f597b73c86bc3ee2
Alex Saft <fend25@gmail.com>
2017-09-05 01:48:43 +0300

    first commit

---
+++ src/components/app.tsx
@@ -0,0 +1,10 @@
+import { h, Component } from 'preact';
+import style from './header/style.css'
+
+export default class App extends Component<any, any> {
+	render() {
+		return (
+			<div id="app">abc</div>
+		)
+	}
+}
---
+++ src/routes/home/index.tsx
@@ -0,0 +1,13 @@
+import { h, Component } from 'preact';
+// import style from './style.css';
+
+export default class Home extends Component<any, any> {
+	render() {
+		return (
+			<div>
+				<h1>Home</h1>
+				<p>This is the Home component.</p>
+			</div>
+		);
+	}
+}
